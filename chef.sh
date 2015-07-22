fce97358a414244c30ff92fafad8e34a1b88a978
Minwoo Lee <ermaker@gmail.com>
2015-07-22 22:52:43 +0900

    Use curl instead of wget

--- chef.sh
+++ chef.sh
@@ -3,7 +3,7 @@
 CHEF_VERSION="0.6.2-1"
 CHEF_DEB=chefdk_${CHEF_VERSION}_amd64.deb
 if ! command -v berks &>/dev/null; then
-  wget "https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/${CHEF_DEB}"
+  \curl -sSL "https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/${CHEF_DEB}" -o $CHEF_DEB
   sudo dpkg -i $CHEF_DEB
   rm -f $CHEF_DEB
 fi
--- vagrant.sh
+++ vagrant.sh
@@ -3,7 +3,7 @@
 VAGRANT_VERSION="1.7.3"
 VAGRANT_DEB=vagrant_${VAGRANT_VERSION}_x86_64.deb
 if ! command -v vagrant &>/dev/null; then
-  wget "https://dl.bintray.com/mitchellh/vagrant/${VAGRANT_DEB}"
+  \curl -sSL "https://dl.bintray.com/mitchellh/vagrant/${VAGRANT_DEB}" -o $VAGRANT_DEB
   sudo dpkg -i $VAGRANT_DEB
   rm -f $VAGRANT_DEB
 fi
