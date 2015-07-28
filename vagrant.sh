e8b1d7d7904675c4038bf40d20bb6f9f836466c3
Minwoo Lee <ermaker@gmail.com>
2015-07-28 17:31:58 +0900

    Update vagrant.sh
    Bump up the vagrant version to 1.7.4

--- vagrant.sh
+++ vagrant.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VAGRANT_VERSION="1.7.3"
+VAGRANT_VERSION="1.7.4"
 VAGRANT_DEB=vagrant_${VAGRANT_VERSION}_x86_64.deb
 if ! command -v vagrant &>/dev/null; then
   \curl -sSL "https://dl.bintray.com/mitchellh/vagrant/${VAGRANT_DEB}" -o $VAGRANT_DEB
